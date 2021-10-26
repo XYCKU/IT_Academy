959be73d138979318918330797b5bb091403d530
XYCKU <48044503+XYCKU@users.noreply.github.com>
2021-10-26 13:38:25 +0300

    task2 comments

--- Homework6/Task2/Program.cs
+++ Homework6/Task2/Program.cs
@@ -8,8 +8,18 @@ namespace Task2
 		static void Main(string[] args)
 		{
 			int[] a = new int[] { 5, 6, 3, 7, 9, 6, 8 };
+			
+			// По возрастанию
 			a.BubbleSort();
 			foreach(var item in a) {
+
+				Console.Write($"{item} ");
+			}
+			Console.WriteLine();
+
+			// По убыванию
+			a.BubbleSort(false);
+			foreach (var item in a) {
 
 				Console.Write($"{item} ");
 			}
