ec9ea3d91a2ce05111b853c914b148b512308e10
XYCKU <48044503+XYCKU@users.noreply.github.com>
2021-12-08 22:48:27 +0300

    beforeEdit

---
+++ Homework14rar/Models/Attributes/AgeAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class AgeAttribute : MyValidationAttribute
+    {
+        private int minAge, maxAge;
+        public int MinAge
+        {
+            get => minAge;
+            private set
+            {
+                minAge = (value >= 0)? value : throw new ArgumentOutOfRangeException(nameof(value));
+            }
+        }
+        public int MaxAge
+        {
+            get => maxAge;
+            private set
+            {
+                maxAge = (value >= 0) ? value : throw new ArgumentOutOfRangeException(nameof(value));
+            }
+        }
+
+        public AgeAttribute() : this(0, 150) { }
+        public AgeAttribute(int minAge, int maxAge)
+        {
+            MinAge = minAge;
+            MaxAge = maxAge;
+        }
+        public override ValidationResult Validate(object item)
+        {
+            int age = (int)item;
+            return (age >= minAge && age <= maxAge)? ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+    }
+}
---
+++ Homework14rar/Models/Attributes/MyValidationAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Attributes
+{
+    public abstract class MyValidationAttribute : ValidationAttribute
+    {
+        public abstract ValidationResult Validate(object item);
+    }
+}
