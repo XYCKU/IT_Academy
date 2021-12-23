d97f6a9239725d6fad3310ded029aa60464c2ec9
XYCKU <48044503+XYCKU@users.noreply.github.com>
2021-12-23 03:08:15 +0300

    practice19 init

---
+++ Practice19/EntryPoint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntryPoint
+{
+    internal class Program
+    {
+        static async Task StringWriterAsync(string str, int amount)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+            while (amount-- > 0)
+            {
+                Console.WriteLine(str);
+                await Task.Delay(100);
+            }
+        } 
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread {Thread.CurrentThread.ManagedThreadId}");
+            var task1 = StringWriterAsync("first", 5);
+            
+            var task2 = StringWriterAsync("second", 5);
+            
+            await Task.WhenAll(task1, task2);
+        }
+    }
+}
